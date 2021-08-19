db3d84569d720338662ed92d37cfd83193c88b00
ihsan1o5 <ihsanullahi501@gmail.com>
2021-08-19 12:14:49 +0500

    levels screen modified for all desktop screens

--- src/containers/Lessons/Lesson.js
+++ src/containers/Lessons/Lesson.js
@@ -7,6 +7,7 @@ import { load_slider } from "../../redux/actions/slider";
 import "./Lesson.css";
 import Reward from '../../assets/images/reward.png';
 import Star from '../../assets/images/star.png';
+import LockIcon from '../../assets/images/lock.png';
 import { MdLock } from 'react-icons/md';
 
 const Lesson = ({ load_config, load_slider, match }) => {
@@ -31,57 +32,57 @@ const Lesson = ({ load_config, load_slider, match }) => {
 
     return (
         <div className="page-bg">
-            <div className="row headr">
-                <div className="col-md-4 header-item">
+            <div className="row ">
+                <div className="col-md-4 mt-3">
                     <div className="level-holder">
                         <h1>Levels</h1>
                     </div>
                 </div>
-                <div className="col-md-4 header-item">
+                <div className="col-md-4 mt-3">
                     <div className="lesson-holder">
                         <h1>Lessons</h1>
                     </div>
                 </div>
-                <div className="col-md-4 header-item">
+                <div className="col-md-4 mt-3">
                     <div className="units-holder">
                         <h1>Units</h1>
                     </div>
                 </div>
             </div>
 
-            <div className="row content">
-                <div className="col-md-2 content-item">
+            <div className="row content content-sm-rw1">
+                <div className="col-md-2">
                     {/* <h1>Button1</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable btn-active" onClick={onClick}>
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item content-item-bottom">
+                <div className="col-md-2 content-item-bottom">
                     {/* <h1>Button3</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     {/* <h1>Button5</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
@@ -89,10 +90,10 @@ const Lesson = ({ load_config, load_slider, match }) => {
             </div>
 
             <div className="row content">
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                         </div>
                     </div>
                     <div className="ratting">
@@ -101,73 +102,73 @@ const Lesson = ({ load_config, load_slider, match }) => {
                         <img src={Star} />
                     </div>
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     {/* <h1>Button2</h1> */}
                 </div>
-                <div className="col-md-2 content-item content-item-bottom">
+                <div className="col-md-2 content-item-bottom">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     {/* <h1>Button4</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     {/* <h1>Button6</h1> */}
                 </div>
             </div>
 
-            <div className="row content">
-                <div className="col-md-2 content-item">
+            <div className="row content content-sm">
+                <div className="col-md-2">
                     {/* <h1>Button1</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item content-item-bottom">
+                <div className="col-md-2 content-item-bottom">
                     {/* <h1>Button3</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     {/* <h1>Button5</h1> */}
                 </div>
-                <div className="col-md-2 content-item">
+                <div className="col-md-2">
                     <div className="bg-clickable">
                         <div className="level-btn">
-                            <img src={Reward} />
+                            <img className="mx-auto d-block" src={Reward} />
                             <div className="lock">
-                                <MdLock className="lock-icon" />
+                                <img className="mx-auto d-block" src={LockIcon} />
                             </div>
                         </div>
                     </div>
